425f5b7ddb7abe8537c07e7909524c14ba9bc77c
Jgone2 <jgoneit@gmail.com>
2023-12-03 17:19:41 +0900

    :tada: :rotating_light: Begin a project

---
+++ src/main/java/com/foriserver/fori/ForiApplication.java
@@ -0,0 +1,13 @@
+package com.foriserver.fori;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/foriserver/fori/security/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.foriserver.fori.security.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@RequiredArgsConstructor
+@Slf4j
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    @ConditionalOnProperty(name = "spring.h2.console.enabled", havingValue = "true")
+    public WebSecurityCustomizer h2ConsoleSecurityCustomizer() {
+        return web -> web.ignoring().requestMatchers(PathRequest.toH2Console());
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        // @ConditionalOnProperty(name = "spring.h2.console.enabled", havingValue = "true") 를 사용하지 않는 경우
+        /*http.authorizeHttpRequests(
+                        request -> request.requestMatchers(PathRequest.toH2Console())
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated())
+                .csrf(csrf -> csrf.ignoringRequestMatchers(PathRequest.toH2Console()))
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));*/
+
+        return http.build();
+    }
+}
