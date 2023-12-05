e382a087cf25da1fc38cd7aa43bd3f8124e910ce
Jgone2 <jgoneit@gmail.com>
2023-12-05 22:11:29 +0900

    :sparkles: createdAt & modifiedAt

--- src/main/java/com/foriserver/fori/common/audit/Auditable.java
+++ src/main/java/com/foriserver/fori/common/audit/Auditable.java
@@ -1,2 +1,31 @@
-package com.foriserver.fori.common.audit;public class Auditable {
+package com.foriserver.fori.common.audit;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class Auditable {
+
+    @CreatedDate
+    @Setter(lombok.AccessLevel.NONE)
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime modifiedAt;
+
+    public Auditable() {
+    }
 }
