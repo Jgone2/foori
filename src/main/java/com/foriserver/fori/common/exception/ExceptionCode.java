9cf0988cd8d5da6c9c90f4b09080f6f5bcf8d687
Jgone2 <jgoneit@gmail.com>
2023-12-05 22:12:11 +0900

    :sparkles: Enum of ExceptionCode

--- src/main/java/com/foriserver/fori/common/exception/ExceptionCode.java
+++ src/main/java/com/foriserver/fori/common/exception/ExceptionCode.java
@@ -1,2 +1,24 @@
-package com.foriserver.fori.common.exception;public enum ExceptionCode {
+package com.foriserver.fori.common.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ExceptionCode {
+
+    // Member
+    MEMBER_NOT_FOUND(404, 10001, "사용자를 찾을 수 없습니다."),
+    MEMBER_EXISTS(409, 10002, "이미 존재하는 사용자입니다."),
+    MEMBER_LOGIN_ID_EXISTS(409, 10003, "이미 존재하는 아이디 입니다."),
+    MEMBER_EMAIL_EXISTS(409, 10004, "이미 존재하는 Email 입니다."),
+
+    // Verify
+    MEMBER_PASSWORD_MISMATCH(409, 10005, "비밀번호가 일치하지 않습니다."),
+    MEMBER_PASSWORD_NOT_CHANGE(409, 10006, "기존의 비밀번호와 같습니다. 비밀번호가 변경되지 않았습니다.");
+
+    private final int status;
+    private final int customCode;
+    private final String message;
+
 }
