bef0d0a5e04309e721989e509aa9d7418b00d299
Jgone2 <jgoneit@gmail.com>
2023-12-15 00:15:52 +0900

    :sparkles:: set MemberRoles & set foundation about customerAuthorityUtils with member role

--- src/main/java/com/foriserver/fori/member/roles/MemberRoles.java
+++ src/main/java/com/foriserver/fori/member/roles/MemberRoles.java
@@ -1,2 +1,20 @@
-package com.foriserver.fori.member.roles;public enum MemberRoles {
+package com.foriserver.fori.member.roles;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum MemberRoles {
+
+    // 관리자 권한
+    ADMIN("ROLE_ADMIN"),
+
+    // 가게 관리자 권한
+    STORE_ADMIN("ROLE_STORE_ADMIN"),
+
+    // 일반 회원 권한
+    MEMBER("ROLE_MEMBER");
+
+    private String memberRole;
 }
--- src/main/java/com/foriserver/fori/security/utils/CustomAuthorityUtils.java
+++ src/main/java/com/foriserver/fori/security/utils/CustomAuthorityUtils.java
@@ -1,2 +1,21 @@
-package com.foriserver.fori.security.utils;public class CustomAuthorityUtils {
+package com.foriserver.fori.security.utils;
+
+import com.foriserver.fori.member.roles.MemberRoles;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+import java.util.List;
+
+@Component
+public class CustomAuthorityUtils {
+
+    private final List<GrantedAuthority> ADMIN_ROLES = AuthorityUtils.createAuthorityList("ROLE_ADMIN", "ROLE_MEMBER");
+    private final List<GrantedAuthority> STORE_ADMIN_ROLES = AuthorityUtils.createAuthorityList("ROLE_STORE_ADMIN", "ROLE_MEMBER");
+    private final List<GrantedAuthority> MEMBER_ROLES = AuthorityUtils.createAuthorityList("ROLE_MEMBER");
+
+
+    public Collection<? extends GrantedAuthority> createAuthorities(List<MemberRoles> roles) {
+    }
 }
