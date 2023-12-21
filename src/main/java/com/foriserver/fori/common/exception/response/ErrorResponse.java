01bce64e1ef04bfa12442255c29c4e369adcaeba
Jgone2 <jgoneit@gmail.com>
2023-12-21 18:43:11 +0900

    :sparkles:: add ErrorResponse

--- src/main/java/com/foriserver/fori/common/exception/response/ErrorResponse.java
+++ src/main/java/com/foriserver/fori/common/exception/response/ErrorResponse.java
@@ -1,2 +1,22 @@
-package com.foriserver.fori.common.exception.response;public class ErrorResponse {
+package com.foriserver.fori.common.exception.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ErrorResponse {
+
+
+    @Getter
+    @AllArgsConstructor
+    public static class FieldError {
+        private String field;
+        private String value;
+        private String reason;
+
+        public static FieldError of(org.springframework.validation.FieldError fieldError) {
+            return new FieldError(fieldError.getField(), fieldError.getRejectedValue().toString(), fieldError.getDefaultMessage());
+        }
+    }
 }
