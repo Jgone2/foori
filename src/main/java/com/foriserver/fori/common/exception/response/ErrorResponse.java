a5414876ea87550d561921b80782dcdd45a45dfb
Jgone2 <jgoneit@gmail.com>
2023-12-27 20:06:44 +0900

    ♻️: add ErrorResponse for AuthenticationFailureHandler

--- src/main/java/com/foriserver/fori/common/exception/response/ErrorResponse.java
+++ src/main/java/com/foriserver/fori/common/exception/response/ErrorResponse.java
@@ -1,12 +1,68 @@
 package com.foriserver.fori.common.exception.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.foriserver.fori.common.exception.CodeEnum.ExceptionCode;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+
+import javax.validation.ConstraintViolation;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Getter
-@AllArgsConstructor
 public class ErrorResponse {
 
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private int status;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private int customCode;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private String message;
+
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
+    private List<FieldError> filedErrors;
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
+    private List<ConstraintViolationError> violationErrors;
+
+    public ErrorResponse(int status, int customCode, String message) {
+        this.status = status;
+        this.customCode = customCode;
+        this.message = message;
+    }
+
+    public ErrorResponse(int status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    public ErrorResponse(List<FieldError> filedErrors, List<ConstraintViolationError> violationErrors) {
+        this.filedErrors = filedErrors;
+        this.violationErrors = violationErrors;
+    }
+
+    public static ErrorResponse of(BindingResult bindingResult) {
+        return new ErrorResponse(FieldError.of(bindingResult), null);
+    }
+
+    public static ErrorResponse of(Set<ConstraintViolation<?>> violations) {
+        return new ErrorResponse(null, ConstraintViolationError.of(violations));
+    }
+
+    // Custom code added
+    public static ErrorResponse of(ExceptionCode exceptionCode) {
+        return new ErrorResponse(exceptionCode.getStatus(), exceptionCode.getCustomCode(), exceptionCode.getMessage());
+    }
+
+    public static ErrorResponse of(HttpStatus httpStatus) {
+        return new ErrorResponse(httpStatus.value(), httpStatus.getReasonPhrase());
+    }
+
+    public static ErrorResponse of(HttpStatus httpStatus, String message) {
+        return new ErrorResponse(httpStatus.value(), message);
+    }
 
     @Getter
     @AllArgsConstructor
@@ -15,8 +71,34 @@ public class ErrorResponse {
         private String value;
         private String reason;
 
-        public static FieldError of(org.springframework.validation.FieldError fieldError) {
-            return new FieldError(fieldError.getField(), fieldError.getRejectedValue().toString(), fieldError.getDefaultMessage());
+        public static List<FieldError> of(BindingResult bindingResult) {
+
+            List<org.springframework.validation.FieldError> fieldErrors = bindingResult.getFieldErrors();
+
+            return fieldErrors.stream()
+                    .map(error -> new FieldError(
+                            error.getField(),
+                            error.getRejectedValue() == null ? "" : error.getRejectedValue().toString(),
+                            error.getDefaultMessage())
+                    ).collect(Collectors.toList());
+        }
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class ConstraintViolationError {
+        private String field;
+        private String value;
+        private String reason;
+
+        public static List<ConstraintViolationError> of(Set<ConstraintViolation<?>> constraintViolations) {
+
+            return constraintViolations.stream()
+                    .map(violation -> new ConstraintViolationError(
+                            violation.getPropertyPath().toString(),
+                            violation.getInvalidValue().toString(),
+                            violation.getMessage())
+                    ).collect(Collectors.toList());
         }
     }
 }
--- src/main/java/com/foriserver/fori/security/handler/MemberAuthenticationFailureHandler.java
+++ src/main/java/com/foriserver/fori/security/handler/MemberAuthenticationFailureHandler.java
@@ -1,34 +1,50 @@
-//package com.foriserver.fori.security.handler;
-//
-//import com.foriserver.fori.common.exception.response.ErrorResponse;
-//import com.google.gson.Gson;
-//import jakarta.servlet.ServletException;
-//import jakarta.servlet.http.HttpServletRequest;
-//import jakarta.servlet.http.HttpServletResponse;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.MediaType;
-//import org.springframework.security.core.AuthenticationException;
-//import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-//
-//import java.io.IOException;
-//
-//@Slf4j
-//public class MemberAuthenticationFailureHandler implements AuthenticationFailureHandler {
-//    @Override
-//    public void onAuthenticationFailure(HttpServletRequest request,
-//                                        HttpServletResponse response,
-//                                        AuthenticationException exception) throws IOException, ServletException {
-//
-//        log.error("# Authentication failed: {}", exception.getMessage());
-//
-//        private void sendErrorResponse(HttpServletResponse response, String errorMessage) throws IOException {
-//
-//            Gson gson = new Gson();
-//            ErrorResponse errorResponse = ErrorResponse.of(HttpStatus.UNAUTHORIZED, errorMessage);    // 401
-//            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-//            response.setStatus(HttpStatus.UNAUTHORIZED.value());
-//            response.getWriter().write(gson.toJson(errorResponse, ErrorResponse.class));
-//        }
-//    }
-//}
+package com.foriserver.fori.security.handler;
+
+import com.foriserver.fori.common.exception.CodeEnum.ExceptionCode;
+import com.foriserver.fori.common.exception.response.ErrorResponse;
+import com.google.gson.Gson;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+
+import java.io.IOException;
+
+@Slf4j
+public class MemberAuthenticationFailureHandler implements AuthenticationFailureHandler {
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request,
+                                        HttpServletResponse response,
+                                        AuthenticationException exception) throws IOException, ServletException {
+
+        log.error("# Authentication failed: {}", exception.getMessage());
+
+        // error 클래스 비지정하는 방식 필요
+        String errorMessage = switch (exception.getClass().getSimpleName()) {
+
+            case "BadCredentialsException", "InternalAuthenticationServiceException", "UsernameNotFoundException"
+                    -> ExceptionCode.BAD_CREDENTIALS.getMessage();
+            case "AuthenticationCredentialsNotFoundException"
+                    -> ExceptionCode.AUTHENTICATION_CREDENTIALS_NOT_FOUND.getMessage();
+
+            default -> ExceptionCode.UNKNOWN_LOGIN_FAIL.getMessage();
+
+        };
+
+        sendErrorResponse(response, errorMessage);
+    }
+
+    private void sendErrorResponse(HttpServletResponse response, String errorMessage) throws IOException {
+
+        Gson gson = new Gson();
+        ErrorResponse errorResponse = ErrorResponse.of(HttpStatus.UNAUTHORIZED, errorMessage);    // 401
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.getWriter().write(gson.toJson(errorResponse, ErrorResponse.class));
+    }
+}
