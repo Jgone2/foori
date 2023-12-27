05429f64210fbaf5e2a10f6a845bb792e95c66d0
Jgone2 <jgoneit@gmail.com>
2023-12-27 20:04:03 +0900

    ✨:: add ExceptionCode for Login

--- src/main/java/com/foriserver/fori/common/exception/CodeEnum/ExceptionCode.java
+++ src/main/java/com/foriserver/fori/common/exception/CodeEnum/ExceptionCode.java
@@ -16,7 +16,12 @@ public enum ExceptionCode {
 
     // Verify
     MEMBER_PASSWORD_MISMATCH(409, 10201, "비밀번호가 일치하지 않습니다."),
-    MEMBER_PASSWORD_NOT_CHANGE(409, 10202, "기존의 비밀번호와 같습니다. 비밀번호가 변경되지 않았습니다.");
+    MEMBER_PASSWORD_NOT_CHANGE(409, 10202, "기존의 비밀번호와 같습니다. 비밀번호가 변경되지 않았습니다."),
+
+    // Login
+    BAD_CREDENTIALS(401, 10301, "아이디 또는 비밀번호가 일치하지 않습니다."),
+    AUTHENTICATION_CREDENTIALS_NOT_FOUND(401, 10302, "인증 정보가 없습니다."),
+    UNKNOWN_LOGIN_FAIL(401, 10302, "알 수 없는 로그인 오류입니다.");
 
     private final int status;
     private final int customCode;
