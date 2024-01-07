9eff5d6dab682e16b004a6708269abc6bece4dfa
Jgone2 <jgoneit@gmail.com>
2024-01-07 20:23:32 +0900

    :sparkles:: service for Image Upload at S3 Bucket Image Server

--- src/main/java/com/foriserver/fori/common/s3/service/S3Service.java
+++ src/main/java/com/foriserver/fori/common/s3/service/S3Service.java
@@ -1,2 +1,21 @@
-package com.foriserver.fori.common.s3.service;public interface S3Service {
+package com.foriserver.fori.common.s3.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+public interface S3Service {
+
+    //  파일 업로드
+    String uploadFile(MultipartFile multipartFile, String dirName);
+    List<String> uploadFiles(List<MultipartFile> multipartFiles, String dirName);
+
+    // S3에 파일 업로드
+    String putS3(MultipartFile multipartFile, String dirName);
+
+    // 사전 서명된 URL 생성 (업로드된 파일의 URL을 얻는다.)
+    String generatePresignedUrl(String fileName);
+
+    // 파일 삭제
+    void deleteFileFromS3(String fileName);
 }
